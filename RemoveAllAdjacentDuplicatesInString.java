1e598c4557f33d1678091e6946d0382624f7293b
rahul-sk <37139616+rahul-sk@users.noreply.github.com>
2020-11-09 15:42:37 +0530

    Update RemoveAllAdjacentDuplicatesInString.java

--- RemoveAllAdjacentDuplicatesInString.java
+++ RemoveAllAdjacentDuplicatesInString.java
@@ -1,3 +1,13 @@
+
+/**
+Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
+We repeatedly make duplicate removals on S until we no longer can. Return the final string after all such duplicate removals have 
+been made.  
+ * @author Rahul S Koimattur
+ * Intuition : Use a Stack and pop the current character if it matches with the top element of the stack, if not then push it to the stack.
+ *             the remaining elements in the stack is the final result. 
+ */
+
 class Solution {
     public String removeDuplicates(String S) {
         Stack<Character> st=new Stack<>();
