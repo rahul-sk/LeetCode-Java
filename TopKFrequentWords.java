7ee183ea6ff20868cf963ea2d3d1737ccf908a43
rahul-sk <37139616+rahul-sk@users.noreply.github.com>
2020-11-17 19:17:11 +0530

    Update TopKFrequentWords.java

--- TopKFrequentWords.java
+++ TopKFrequentWords.java
@@ -2,10 +2,13 @@
  * Given a non-empty list of words, return the k most frequent elements. Your answer should be sorted by frequency from highest to lowest. 
  * If two words have the same frequency, then the word with the lower alphabetical order comes first.
  * @author Rahul S Koimattur
- * Intuition :  Use Bucket Sort Algorithm, count the frequency of words useing a HashMap, then sort the contents of each bucket to handel multiple words
-                with same frequency. Finally iterate from the last bucket down to the first and append the respective words into a result List.
+ * Method 1 Intuition :  Use Bucket Sort Algorithm, count the frequency of words using a HashMap, then sort the contents of each bucket to handel multiple words
+ *                       with same frequency. Finally iterate from the last bucket down to the first and append the respective words into a result List.
+ *
+ * Method 2 Intuition :  count the frequency of words useing a HashMap, then use PriorityQueue and sort the entries based on frequency.
  */
  
+// Method 1 code :
  class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         Map<String,Integer> hm=new HashMap<>();
@@ -37,5 +40,40 @@
         }
         return res;
         
+    }
+}
+
+// Method 2 code :
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> hm=new HashMap<>();
+        for(String s:words){
+           hm.put(s,hm.getOrDefault(s,0)+1);
+        }
+      PriorityQueue<Map.Entry<String,Integer>> q=new PriorityQueue<>(new Comparator<>(){
+      public int compare(Map.Entry<String,Integer> s1,Map.Entry<String,Integer> s2){
+         if(s1.getValue()==s2.getValue()){
+             if(s1.getKey().compareTo(s2.getKey())<=0){
+                 return -1;
+             }
+             else{
+               return 1;
+             }
+         }
+         return s2.getValue()-s1.getValue();
+      }
+        });
+        q.addAll(hm.entrySet());
+        List<String> res=new ArrayList<>();
+        while(!q.isEmpty()){
+          Map.Entry e=q.poll();
+          if(k>0){
+            res.add((String)e.getKey());
+            k--;
+          }  
+
+        }
+        return res;
     }
 }
