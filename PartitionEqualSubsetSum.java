5aa00715e27a4df32ef65a2e2d843f2145746743
rahul-sk <37139616+rahul-sk@users.noreply.github.com>
2020-12-02 15:04:50 +0530

    Update PartitionEqualSubsetSum.java

--- PartitionEqualSubsetSum.java
+++ PartitionEqualSubsetSum.java
@@ -14,18 +14,18 @@
         sum/=2;
         int n=arr.length;
         boolean[][] dp=new boolean[n+1][sum+1];
-        dp[0][0]=true;
+        dp[0][0]=true; // If the sum is zero and the array is empty, we can get to a solution by doing nothing and hence we declare it as true
         for(int i=1;i<n+1;i++){
-        dp[i][0]=true;
+        dp[i][0]=true;  // If the sum is zero, we can get it by doing nothing irrespective of the array elements and hence is true;
         }
         for(int j=1;j<sum+1;j++){
-         dp[0][j]=false;
+         dp[0][j]=false; // If the array is empty, we cannot produce any sum value and hence is false.
         }
         for(int i=1;i<n+1;i++){
         for(int j=1;j<sum+1;j++){
            dp[i][j]=dp[i-1][j];
             if(arr[i-1]<=j){
-            dp[i][j]=dp[i][j]||dp[i-1][j-arr[i-1]];
+            dp[i][j]=dp[i][j]||dp[i-1][j-arr[i-1]]; // If the current array element is less than or equal to the current target, we can either consider it or not consider it.
             }
         }
         }
